47d24beb411acfe97261152ecdc08f61caaea9a3
Filip Frank <ffrank@tmns.com>
2018-09-05 16:52:30 +0200

    Adding multiple types

--- dsl/generate.py
+++ dsl/generate.py
@@ -12,11 +12,13 @@ from dsl.root import SRC_DIR
 
 def main(location="", filename="", output="", debug=False):
 
-    model_type = execute(os.path.join(SRC_DIR, "language"), 'type.tx', 'laptop.rbt', debug, debug)
+    model_type = execute(os.path.join(SRC_DIR, "language"), 'type.tx', 'types.rbt', debug, debug)
     model = execute(os.path.join(SRC_DIR, "language"), 'products_mining.tx', 'laptop_program.rbt', debug, debug)
     
     generator = SpiderGenerator(model)
     generator.generate_application(output)
+    
+    
 
 
 if __name__ == '__main__':
