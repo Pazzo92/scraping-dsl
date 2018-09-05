6e1bd6579516e21596260c15f4a9274826de954d
Filip Frank <ffrank@tmns.com>
2018-09-05 13:39:34 +0200

    Renaming grammar files

--- dsl/generate.py
+++ dsl/generate.py
@@ -12,7 +12,7 @@ from dsl.root import SRC_DIR
 
 def main(location="", filename="", output="", debug=False):
 
-    model = execute(os.path.join(SRC_DIR, "language"), 'real_deal.tx', 'gitara_program.rbt', debug, debug)
+    model = execute(os.path.join(SRC_DIR, "language"), 'product_mining.tx', 'gitara_program.rbt', debug, debug)
 
     generator = SpiderGenerator(model)
     generator.generate_application(output)
